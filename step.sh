c52598d6bf31e2b1ac8b977c174cbde5046f944c
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-06-13 11:40:32 +0200

    update cocoapods to Podfile.lock specified version

--- step.sh
+++ step.sh
@@ -5,11 +5,11 @@ source "${THIS_SCRIPTDIR}/_bash_utils/utils.sh"
 source "${THIS_SCRIPTDIR}/_bash_utils/formatted_output.sh"
 
 # Validate parameters
-echo "Configs:"
-echo "  * source_root_path: $source_root_path"
-echo "  * podfile_path: $podfile_path"
-echo "  * [deprecated!] is_update_cocoapods: $is_update_cocoapods"
-echo "  * install_cocoapods_version: $install_cocoapods_version"
+echo_string_to_formatted_output "Configs:"
+echo_string_to_formatted_output "  * source_root_path: $source_root_path"
+echo_string_to_formatted_output "  * podfile_path: $podfile_path"
+echo_string_to_formatted_output "  * [deprecated!] is_update_cocoapods: $is_update_cocoapods"
+echo_string_to_formatted_output "  * install_cocoapods_version: $install_cocoapods_version"
 
 if [ -z "${source_root_path}" ]; then
   write_section_to_formatted_output "# Error"
@@ -17,15 +17,41 @@ if [ -z "${source_root_path}" ]; then
   exit 1
 fi
 print_and_do_command_exit_on_error cd "${source_root_path}"
+echo
+
+# Update cocoapods
+update_version=""
 
 if [ -n "${install_cocoapods_version}" ] ; then
-  if [[ "${install_cocoapods_version}" == "latest" ]] ; then
-    # Install latest Cocoapods version
+  update_version="${install_cocoapods_version}"
+elif [[ "${is_update_cocoapods}" != "false" ]] ; then
+  # Deprecated - Install latest Cocoapods version
+  echo_string_to_formatted_output "[!] is_update_cocoapods is deprecated, use install_cocoapods_version input instead of this."
 
-    print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/_steps-cocoapods-update/step.sh"
+  update_version="latest"
+else
+  version=""
+  if [ -f "$source_root_path/Podfile.lock" ] ; then
+    # COCOAPODS: 1.0.0
+    regex="COCOAPODS: (.+)"
+    while read line; do
+      if [[ $line =~ $regex ]] ; then
+        version="${BASH_REMATCH[1]}"
+      fi
+    done < "$source_root_path/Podfile.lock"
+  fi
+
+  if [ -n "$version" ] ; then
+    update_version="$version"
+  fi
+fi
+
+if [ -n "$update_version" ] ; then
+  echo_string_to_formatted_output "update cocoapods to: $update_version"
+
+  if [[ "${update_version}" == "latest" ]] ; then
+     print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/_steps-cocoapods-update/step.sh"
   else
-    # Install desired Cocoapods version
-
     # "gem uninstall" is required if you want to use
     # an *older* cocoapods version than the preinstalled one
     # note: gem treats pre-release versions as older than
@@ -35,15 +61,6 @@ if [ -n "${install_cocoapods_version}" ] ; then
     # install the version you want to use
     print_and_do_command_exit_on_error gem install cocoapods --version "${install_cocoapods_version}" --no-document
     print_and_do_command_exit_on_error pod setup --verbose
-  fi
-else
-  # Deprecated - Install latest Cocoapods version
-  if [[ "${is_update_cocoapods}" != "false" ]] ; then
-    echo
-    echo "[!] is_update_cocoapods is deprecated, use install_cocoapods_version input instead of this."
-    echo
-
-    print_and_do_command_exit_on_error bash "${THIS_SCRIPTDIR}/_steps-cocoapods-update/step.sh"
   fi
 fi
 
