c3698778e36aa81e65e246e818d7ea61ceb67456
Richard Bogdan <richard.bogdan@bitrise.io>
2019-11-08 12:48:50 +0100

    TOOL-30 Dep update.

--- vendor/github.com/bitrise-io/bitrise-init/steps/const.go
+++ vendor/github.com/bitrise-io/bitrise-init/steps/const.go
@@ -18,7 +18,7 @@ const (
 	// AndroidUnitTestID ...
 	AndroidUnitTestID = "android-unit-test"
 	// AndroidUnitTestVersion ...
-	AndroidUnitTestVersion = "0.11.1"
+	AndroidUnitTestVersion = "1.0.0"
 )
 
 const (
@@ -32,21 +32,21 @@ const (
 	// GitCloneID ...
 	GitCloneID = "git-clone"
 	// GitCloneVersion ...
-	GitCloneVersion = "4.0.14"
+	GitCloneVersion = "4.0.17"
 )
 
 const (
 	// CachePullID ...
 	CachePullID = "cache-pull"
 	// CachePullVersion ...
-	CachePullVersion = "2.0.1"
+	CachePullVersion = "2.1.1"
 )
 
 const (
 	// CachePushID ...
 	CachePushID = "cache-push"
 	// CachePushVersion ...
-	CachePushVersion = "2.2.0"
+	CachePushVersion = "2.2.1"
 )
 
 const (
@@ -67,7 +67,7 @@ const (
 	// DeployToBitriseIoID ...
 	DeployToBitriseIoID = "deploy-to-bitrise-io"
 	// DeployToBitriseIoVersion ...
-	DeployToBitriseIoVersion = "1.6.0"
+	DeployToBitriseIoVersion = "1.9.2"
 )
 
 const (
@@ -83,35 +83,35 @@ const (
 	// SignAPKID ...
 	SignAPKID = "sign-apk"
 	// SignAPKVersion ...
-	SignAPKVersion = "1.3.1"
+	SignAPKVersion = "1.4.1"
 )
 
 const (
 	// InstallMissingAndroidToolsID ...
 	InstallMissingAndroidToolsID = "install-missing-android-tools"
 	// InstallMissingAndroidToolsVersion ...
-	InstallMissingAndroidToolsVersion = "2.3.5"
+	InstallMissingAndroidToolsVersion = "2.3.7"
 )
 
 const (
 	// FastlaneID ...
 	FastlaneID = "fastlane"
 	// FastlaneVersion ...
-	FastlaneVersion = "2.4.0"
+	FastlaneVersion = "2.6.0"
 )
 
 const (
 	// CocoapodsInstallID ...
 	CocoapodsInstallID = "cocoapods-install"
 	// CocoapodsInstallVersion ...
-	CocoapodsInstallVersion = "1.7.2"
+	CocoapodsInstallVersion = "1.9.1"
 )
 
 const (
 	// CarthageID ...
 	CarthageID = "carthage"
 	// CarthageVersion ...
-	CarthageVersion = "3.2.1"
+	CarthageVersion = "3.2.2"
 )
 
 const (
@@ -125,14 +125,14 @@ const (
 	// XcodeArchiveID ...
 	XcodeArchiveID = "xcode-archive"
 	// XcodeArchiveVersion ...
-	XcodeArchiveVersion = "2.5.0"
+	XcodeArchiveVersion = "2.7.0"
 )
 
 const (
 	// XcodeTestID ...
 	XcodeTestID = "xcode-test"
 	// XcodeTestVersion ...
-	XcodeTestVersion = "2.3.2"
+	XcodeTestVersion = "2.4.1"
 )
 
 const (
@@ -146,7 +146,7 @@ const (
 	// NugetRestoreID ...
 	NugetRestoreID = "nuget-restore"
 	// NugetRestoreVersion ...
-	NugetRestoreVersion = "1.0.7"
+	NugetRestoreVersion = "1.1.0"
 )
 
 const (
@@ -160,14 +160,14 @@ const (
 	// XamarinArchiveID ...
 	XamarinArchiveID = "xamarin-archive"
 	// XamarinArchiveVersion ...
-	XamarinArchiveVersion = "1.5.0"
+	XamarinArchiveVersion = "1.5.1"
 )
 
 const (
 	// XcodeArchiveMacID ...
 	XcodeArchiveMacID = "xcode-archive-mac"
 	// XcodeArchiveMacVersion ...
-	XcodeArchiveMacVersion = "1.7.0"
+	XcodeArchiveMacVersion = "1.8.0"
 )
 
 const (
@@ -181,14 +181,14 @@ const (
 	// CordovaArchiveID ...
 	CordovaArchiveID = "cordova-archive"
 	// CordovaArchiveVersion ...
-	CordovaArchiveVersion = "2.0.0"
+	CordovaArchiveVersion = "2.1.0"
 )
 
 const (
 	// IonicArchiveID ...
 	IonicArchiveID = "ionic-archive"
 	// IonicArchiveVersion ...
-	IonicArchiveVersion = "2.0.0"
+	IonicArchiveVersion = "2.1.0"
 )
 
 const (
@@ -216,7 +216,7 @@ const (
 	// NpmID ...
 	NpmID = "npm"
 	// NpmVersion ...
-	NpmVersion = "1.0.2"
+	NpmVersion = "1.1.0"
 )
 
 const (
@@ -230,7 +230,7 @@ const (
 	// YarnID ...
 	YarnID = "yarn"
 	// YarnVersion ...
-	YarnVersion = "0.0.8"
+	YarnVersion = "0.1.0"
 )
 
 const (
@@ -251,12 +251,12 @@ const (
 	// FlutterAnalyzeID ...
 	FlutterAnalyzeID = "flutter-analyze"
 	// FlutterAnalyzeVersion ...
-	FlutterAnalyzeVersion = "0.1.0"
+	FlutterAnalyzeVersion = "0.1.1"
 )
 
 const (
 	// FlutterBuildID ...
 	FlutterBuildID = "flutter-build"
 	// FlutterBuildVersion ...
-	FlutterBuildVersion = "0.9.2"
+	FlutterBuildVersion = "0.12.0"
 )
